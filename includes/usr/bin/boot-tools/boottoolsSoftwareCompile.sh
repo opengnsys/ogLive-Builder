adfc3f261798ab9b8fa45aa4111c3b00922f454e
Jose M. Guisado <jguisado@soleta.eu>
2022-04-25 17:25:52 +0200

    Add python-libfdisk
    python-libfdisk is a python binding for libfdisk library.
    
    The bindings are used by new versions of ogClient.
    
    Prior versions of ogClient do not break when running inside a new
    ogLive.
    
    http://git.48k.eu/python-libfdisk/

--- includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
+++ includes/usr/bin/boot-tools/boottoolsSoftwareCompile.sh
@@ -19,6 +19,14 @@ which ms-sys || (wget https://sourceforge.net/projects/ms-sys/files/latest/downl
 echo  "spartlnx"
 which spartlnx.run || $(wget http://damien.guibouret.free.fr/savepart.zip &>/dev/null; unzip -o savepart.zip -d /sbin/)
 
+echo "python-libfdisk"
+apt-get -y install python3-psutil python3-dev libfdisk-dev python3-setuptools
+git clone git://git.48k.eu/python-libfdisk
+pushd python-libfdisk
+python3 setup.py install
+popd
+apt-get -y remove python3-dev python3-setuptools
+
 popd
 export LANGUAGE=$OLDLANGUAGE
 export LC_ALL=$OLDLC_ALL
